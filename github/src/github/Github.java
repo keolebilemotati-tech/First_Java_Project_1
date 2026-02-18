40a3ec1bdba3912613e06271310ad1cf866ef1e1
Keolebile <Keolebile@LAPTOP-FK3UUHKG>
2026-02-18 11:47:50 +0200

    my initial commit and added print statements

---
+++ github/src/github/Github.java
@@ -0,0 +1,24 @@
+
+package github;
+
+
+public class Github {
+/*This is a 
+    multi-line
+    comment 
+    */
+    
+    //What is a string 
+    //String is array of ccharacters 
+    //john
+    //[j][o][h][n]
+    //[x0001][x002]...etc. 
+    
+    public static void main(String[] args) {
+    System.out.println("My name is John"); 
+    System.out.print("I am funny");
+    System.out.println("Clown!");
+    System.out.println("This has been fun!");
+    }
+    
+}
